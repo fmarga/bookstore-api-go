23820c4844d041c2938a21bb11460feb032d7303
fmarga <marga.fmartins@gmail.com>
2023-02-24 10:28:28 -0300

    feat: add content-type middleware

---
+++ mid/middleware.go
@@ -0,0 +1,10 @@
+package mid
+
+import "net/http"
+
+func ContenType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/fmarga/bookstore-api-go/controllers"
+	"github.com/fmarga/bookstore-api-go/mid"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(mid.ContenType)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/all", controllers.AllBooks).Methods("Get")
 	r.HandleFunc("/api/all/{id}", controllers.Book).Methods("Get")
